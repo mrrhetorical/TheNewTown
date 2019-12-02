931031d024aeaa0d5a18e7e0f1421760658109a3
Caleb Brock <caleb.brock@broshin.com>
2019-12-01 23:26:38 -0800

    Added flags enum

---
+++ src/main/java/com/rhetorical/town/towns/flags/TownFlag.java
@@ -0,0 +1,8 @@
+package com.rhetorical.town.towns.flags;
+
+public enum TownFlag {
+	NO_PVP, // Disables pvp within the town border. (default: true)
+	ALLOW_MODIFICATION, // Allows residents to break and destroy blocks in plots they are not leasing. (default: true)
+	ALLOW_PICKUP, // Allows picking up items by non residents within the town borders. (default true)
+	MOB_SPAWNING, // Disables mob spawning within the town (default: false)
+}
