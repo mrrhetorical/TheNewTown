873a838fc4cfa079edf8aaddf849828c57355ea7
Caleb Brock <caleb.brock@broshin.com>
2019-12-01 21:34:52 -0800

    Cleaned up events for listening to block placing and breaking

--- src/main/java/com/rhetorical/town/towns/PlotListener.java
+++ src/main/java/com/rhetorical/town/towns/PlotListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+@SuppressWarnings("Duplicates")
 class PlotListener implements Listener {
 
 	private Plot plot;
@@ -46,12 +47,12 @@ class PlotListener implements Listener {
 
 	@EventHandler
 	public void onPlayerBreakBlock(BlockBreakEvent e) {
-		if (!getPlot().isInPlot(e.getPlayer().getLocation()) && !getPlot().isInPlot(e.getBlock().getLocation()))
+		if (!getPlot().isInPlot(e.getBlock().getLocation()))
 			return;
 
 		Town town = TownManager.getInstance().getTown(plot.getTown());
 
-		if ((getPlot().getLeaser() != null && !e.getPlayer().getUniqueId().equals(getPlot().getLeaser())) && !e.getPlayer().getUniqueId().equals(town.getMayor())) {
+		if (getPlot().getLeaser() != null && !e.getPlayer().getUniqueId().equals(getPlot().getLeaser()) && !e.getPlayer().getUniqueId().equals(town.getMayor())) {
 			e.setCancelled(true);
 			return;
 		}
@@ -64,12 +65,12 @@ class PlotListener implements Listener {
 
 	@EventHandler
 	public void onPlayerPlaceBlockEvent(BlockPlaceEvent e) {
-		if (!getPlot().isInPlot(e.getPlayer().getLocation()) && !getPlot().isInPlot(e.getBlock().getLocation()))
+		if (!getPlot().isInPlot(e.getBlock().getLocation()))
 			return;
 
 		Town town = TownManager.getInstance().getTown(plot.getTown());
 
-		if ((getPlot().getLeaser() != null && !e.getPlayer().getUniqueId().equals(getPlot().getLeaser())) && !e.getPlayer().getUniqueId().equals(town.getMayor())) {
+		if (getPlot().getLeaser() != null && !e.getPlayer().getUniqueId().equals(getPlot().getLeaser()) && !e.getPlayer().getUniqueId().equals(town.getMayor())) {
 			e.setCancelled(true);
 			return;
 		}
